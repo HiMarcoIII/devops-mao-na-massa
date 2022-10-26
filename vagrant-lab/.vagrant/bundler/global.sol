57e1209e3a4ed2b40b1d60f33e8be3e1f26aacbb
Marco Aurélio Silva Gonçalves <marco.goncalves@fesfsus.ba>
2022-10-26 07:53:39 -0300

    primeiro commit - lab vagrant

---
+++ vagrant-lab/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"f0eb96660f3a14fd92b05ec94f00c4b07691f8693daed233e138a0c85ad767e7","vagrant_version":"2.3.2"}
